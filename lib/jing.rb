5938a8f954be2cc3a668ffe0a54f16e57f4a217d
sshaw <skye.shaw@gmail.com>
2013-09-04 22:59:23 -0400

    make schema a constructor arg

--- lib/jing.rb
+++ lib/jing.rb
@@ -14,19 +14,22 @@ class Jing
     on :compact,  "-c",   Optout::Boolean
     on :encoding, "-e",   String
     on :id_check, "-i",   Optout::Boolean,     :default  => false
-    on :rngfile,          Optout::File.exists, :required => true
+    on :schema,           Optout::File.exists, :required => true
     on :xmlfile,          Optout::File.exists, :required => true
   end
 
-
-
   ##
-  # === Arguments 
+  # Cretae an instance to validate against the given schema. The schema can be in the XML or compact syntax.
   #
-  # [options (Hash)] Jing options. Optional.
+  #  jing = Jing.new("schema.rng", options)
+  #
+  # === Arguments
+  #
+  # [schema (String)] Path to the schema
+  # [options (Hash)] Jing options, optional
   #
   # === Options
-  #  
+  #
   # [:java (String)] Name and/or location of the java executable. Defaults to <code>"java"</code>.
   # [:jar (String)] Path to the Jing JAR file. Defaults to the bundled JAR.
   # [:compact (Boolean)] Set to +true+ if the schema uses the RELAX NG compact syntax. Defaults to false, will be set to +true+ is the schema has a +.rnc+ extension.
@@ -37,50 +40,48 @@ class Jing
   #
   # [ArgumentError] If the options are not +nil+ or a +Hash+.
 
-  def initialize(options = nil)
+  def initialize(schema, options = nil)
     if options
       raise ArgumentError, "options must be a Hash" unless Hash === options
       @options = options.dup
     end
 
     @options ||= {}
-    # Optout quirk: true will *include* the switch, which means we *don't* want to check 
+    @options[:schema] = schema
+    @options[:compact] = true if @options[:compact].nil? and @options[:schema] =~ /\.rnc\Z/i   # Don't override an explicit setting
+    # Optout quirk: true will *include* the switch, which means we *don't* want to check
     @options[:id_check] = !@options[:id_check] if @options.include?(:id_check)
   end
 
   ##
-  # Validate an XML document against a RELAX NG schema file. The schema can be in the XML or the compact syntax.
+  # Validate an XML document against the schema.
   #
-  #  jing = Jing.new(options)
-  #  jing.validate("schema.rng", "doc.xml")
+  #  errors = jing.validate("doc.xml")
   #
-  # === Arguments 
+  # === Arguments
   #
-  # [rng (String)] Path the RELAX NG schema file
   # [xml (String)] Path to the XML file
   #
   # === Errors
   #
-  # [Jing::OptionError] A Jing option was invalid. Note that this <b>does not apply to an invalid <code>:java</code> option.</b> 
+  # [Jing::OptionError] A Jing option was invalid. Note that this <b>does not apply to an invalid <code>:java</code> option.</b>
   # [Jing::ExecutionError] Problems were encountered trying to execute Jing.
   #
   # === Returns
   #
   # [Array] The errors, each element is a +Hash+. See Error Hash for more info.
-  # 
+  #
   # ==== Error Hash
   #
   # The error hash contains the following keys/values
   #
-  # [:file (String)] File that contained the error. Can be the schema or the instance XML.
+  # [:source (String)] File containing the error, can be the schema or the instance XML.
   # [:line (Fixnum)] Line number
   # [:column (Fixnum)] Column number
   # [:message (String)] The problem
-  
-  def validate(rng, xml)
-    @options[:compact] = true if @options[:compact].nil? and rng =~ /\.rnc\Z/i   # Don't override an explicit setting
-    @options[:rngfile] = rng
-    @options[:xmlfile] = xml    
+
+  def validate(xml)
+    @options[:xmlfile] = xml
 
     out = execute(@options)
     return [] if $?.success? and out.empty?
@@ -89,6 +90,28 @@ class Jing
     errors
   end
 
+  # Validate an XML document against the schema. To receive a list of vlidation errors use #validate.
+  #
+  #  puts "yay!" if jing.valid?("doc.xml")
+  #
+  # === Arguments
+  #
+  # [xml (String)] Path to the XML file
+  #
+  # === Errors
+  #
+  # Same as #validate
+  #
+  # === Returns
+  # 
+  # [Boolean] +true+ if valid, +false+ if invalid
+  #
+  
+  def valid?(xml)
+    errors = validate(xml)
+    errors.none?
+  end
+    
   private
   def execute(options)
     cmd = @@option_builder.shell(options)
@@ -103,12 +126,12 @@ class Jing
     errors = []
     output.split("\n").map do |line|
       if line =~ /\A(.+):(\d+):(\d+):\s+\w+:\s+(.+)\Z/
-        errors << {
-          :file    => $1,
-          :line    => $2.to_i,
-          :column  => $3.to_i,
-          :message => $4
-        }
+	errors << {
+	  :source  => $1,
+	  :line    => $2.to_i,
+	  :column  => $3.to_i,
+	  :message => $4
+	}
       end
     end
     errors
--- test/test_jing.rb
+++ test/test_jing.rb
@@ -11,97 +11,105 @@ class TestJing < MiniTest::Unit::TestCase
   RNC_SCHEMA = File.join(root, "schema.rnc")
 
   def test_default_jar_file_used
-    cmd = fakeshell { Jing.new.validate(RNG_SCHEMA, VALID_XML) }
+    cmd = fakeshell { Jing.new(RNG_SCHEMA).validate(VALID_XML) }
     assert_match /\A'java'\s+ -jar\s+ '#{Jing::DEFAULT_JAR}' \s+/x, cmd
   end
 
   def test_jar_file_must_exist
     assert_raises Jing::OptionError, /\bjar\b/ do
-      Jing.new(:jar => "__oopsy__!").validate(RNG_SCHEMA, VALID_XML)
+      Jing.new(RNG_SCHEMA, :jar => "__oopsy__!").validate(VALID_XML)
     end
   end
 
   def test_jar_option
     jar = Tempfile.new "jar"
-    cmd = fakeshell { Jing.new(:jar => jar.path).validate(RNG_SCHEMA, VALID_XML) }
+    cmd = fakeshell { Jing.new(RNG_SCHEMA, :jar => jar.path).validate(VALID_XML) }
     assert_match /\A'java'\s+ -jar\s+ '#{jar.path}' \s+/x, cmd
   end
 
   def test_java_option
     java = "/usr/alt/java"
-    cmd = fakeshell { Jing.new(:java => java).validate(RNG_SCHEMA, VALID_XML) }
+    cmd = fakeshell { Jing.new(RNG_SCHEMA, :java => java).validate(VALID_XML) }
     assert_match /\A'#{java}'\s+/, cmd
   end
 
   def test_java_must_exist
     # This should be an OptionError!
     assert_raises Jing::ExecutionError, /\bjava\b/ do
-      Jing.new(:java => "__no_no_no__").validate(RNG_SCHEMA, VALID_XML)
+      Jing.new(RNG_SCHEMA, :java => "__no_no_no__").validate(VALID_XML)
     end
   end
 
   def test_encoding_option
     enc = "iso-8859-1"
-    cmd = fakeshell { Jing.new(:encoding => enc).validate(RNG_SCHEMA, VALID_XML) }
+    cmd = fakeshell { Jing.new(RNG_SCHEMA, :encoding => enc).validate(VALID_XML) }
     assert_match /\A'java'\s+ -jar\s+ '#{Jing::DEFAULT_JAR}'\s+ -e\s+ '#{enc}'/x, cmd
   end
 
   def test_id_check_option
-    cmd = fakeshell { Jing.new(:id_check => false).validate(RNG_SCHEMA, VALID_XML) }
+    cmd = fakeshell { Jing.new(RNG_SCHEMA, :id_check => false).validate(VALID_XML) }
     assert_match /\A'java'\s -jar\s+ '#{Jing::DEFAULT_JAR}'\s+ -i/x, cmd
 
-    cmd = fakeshell { Jing.new(:id_check => true).validate(RNG_SCHEMA, VALID_XML) }
+    cmd = fakeshell { Jing.new(RNG_SCHEMA, :id_check => true).validate(VALID_XML) }
     refute_match /\b-i\b/x, cmd
 
-    cmd = fakeshell { Jing.new.validate(RNG_SCHEMA, VALID_XML) }
+    cmd = fakeshell { Jing.new(RNG_SCHEMA).validate(VALID_XML) }
     refute_match /\b-i\b/x, cmd
   end
 
   def test_compact_option
-    cmd = fakeshell { Jing.new(:compact => true).validate(RNC_SCHEMA, VALID_XML) }
+    cmd = fakeshell { Jing.new(RNC_SCHEMA, :compact => true).validate(VALID_XML) }
     assert_match /\A'java'\s -jar\s+ '#{Jing::DEFAULT_JAR}'\s+ -c/x, cmd
 
-    cmd = fakeshell { Jing.new(:compact => false).validate(RNC_SCHEMA, VALID_XML) }
+    cmd = fakeshell { Jing.new(RNC_SCHEMA, :compact => false).validate(VALID_XML) }
     refute_match /\b-c\b/, cmd
   end
 
   def test_compact_option_when_compact_schema_is_used
-    cmd = fakeshell { Jing.new.validate(RNC_SCHEMA, VALID_XML) }
+    cmd = fakeshell { Jing.new(RNC_SCHEMA).validate(VALID_XML) }
     assert_match /\A'java'\s+ -jar\s+ '#{Jing::DEFAULT_JAR}'\s+ -c/x, cmd
   end
 
   def test_relaxng_file_must_exist
     assert_raises Jing::OptionError, /cannot read/ do
-      Jing.new.validate("bad_file_name", VALID_XML)
+      Jing.new("bad_file_name").validate(VALID_XML)
     end
   end
 
   def test_instance_xml_file_must_exist
     assert_raises Jing::OptionError, /cannot read/ do
-      Jing.new.validate(VALID_XML, "bad_file_name")
+      Jing.new(VALID_XML).validate("bad_file_name")
     end
   end
 
-  def test_valid_instance_xml_returns_no_errors
-    errors = Jing.new.validate(RNG_SCHEMA, VALID_XML)
-    assert_equal 0, errors.size
+  def test_valid_instance_xml_returns_true
+    assert Jing.new(RNG_SCHEMA).valid?(VALID_XML)
   end
 
-  def test_invalid_instance_xml_errors_are_parsed
-    errors = Jing.new.validate(RNG_SCHEMA, INVALID_XML)
+  def test_valid_instance_xml_returns_no_errors
+    errors = Jing.new(RNG_SCHEMA).validate(VALID_XML)
+    assert_equal 0, errors.size    
+  end
+
+  def test_invalid_instance_xml_returns_false
+    assert !Jing.new(RNG_SCHEMA).valid?(INVALID_XML)
+  end
+
+  def test_invalid_instance_xml_returns_errors
+    errors = Jing.new(RNG_SCHEMA).validate(INVALID_XML)
     assert_equal 1, errors.size
 
     err = errors[0]
-    assert_equal INVALID_XML, err[:file]
+    assert_equal INVALID_XML, err[:source]
     assert_equal 4, err[:line]
     assert_match /\A\d\d?\z/, err[:column].to_s
     assert_match /\bemail\b/, err[:message]
   end
 
-  def test_successful_command_with_un_parsable_output_raises_an_exception
+  def test_successful_exit_status_with_unparsable_output_raises_an_exception
     output = "something bad happened"
     assert_raises Jing::ExecutionError, /#{output}/ do
-      fakeshell(:exit => 0, :output => output) { Jing.new.validate(RNC_SCHEMA, VALID_XML) }
+      fakeshell(:exit => 0, :output => output) { Jing.new(RNG_SCHEMA).validate(VALID_XML) }
     end
   end
 
